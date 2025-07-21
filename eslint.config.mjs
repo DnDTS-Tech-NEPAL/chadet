23aa7b3eb7b9ddf9c4f6ee0bbfcedb19a5a586e9
alishabirat <alishabirat@gmail.com>
2025-07-21 11:40:47 +0545

    setup project with providers, font configuration, and essential dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends
+  ("next/core-web-vitals", "next/typescript",
+    "plugin:prettier/recommended",
+  ),
+  {
+    rules: {
+      "no-console": "error",
+      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
+  }
 ];
 
 export default eslintConfig;
