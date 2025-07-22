25b72b28a770834b44501536bcca5e9345f8467e
alishabirat <alishabirat@gmail.com>
2025-07-22 21:25:47 +0545

    add barrel eport to svg and import icon from figma file
    ->update topbar taking bg color from variable

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,7 @@
+export * from "./facebook.svg";
+export * from "./instagram.svg";
+export * from "./twitter.svg";
+export * from "./search.svg";
+export * from "./cart.svg";
+export * from "./wishlist.svg";
+export * from "./user.svg";
--- src/components/layout/topbar/(components)/TopBanner.tsx
+++ src/components/layout/topbar/(components)/TopBanner.tsx
@@ -2,9 +2,9 @@ import { Box, Text } from "@chakra-ui/react";
 
 export const TopBanner = () => {
   return (
-    <Box bg="#4E4934" color="white" py={1} textAlign="center">
-      <Text fontSize="sm">
-        <b>FREE $76 GIFT</b> ON ALL ORDERS $80 +AUD
+    <Box bg="brand.500" py={2.5} textAlign="center">
+      <Text fontSize="xs" color="white">
+        <b>FREE $76 GIFT </b> ON ALL ORDERS $80 +AUD
       </Text>
     </Box>
   );
