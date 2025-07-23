af24c28f1c30a0b551e43856bb4fc1e5d545e77b
alishabirat <alishabirat@gmail.com>
2025-07-23 10:36:13 +0545

    update
    ->barrel export for homepage and import homepagecard and quotesection in page.tsx

--- src/app/(web)/home/index.tsx
+++ src/app/(web)/home/index.tsx
@@ -1,10 +1,16 @@
-import { HeroSection } from "./(components)";
+import { HeroSection, HomepageCards, QuoteSection } from "./(components)";
 import { useConfigQuery } from "@/hooks/api";
 
 export const DefaultLandingPage = () => {
   const { data: config, isLoading } = useConfigQuery();
 
-  if (isLoading || !config) return null; 
+  if (isLoading || !config) return null;
 
-  return <HeroSection />;
+  return (
+    <>
+      <HeroSection />
+      <HomepageCards />
+      <QuoteSection />
+    </>
+  );
 };
