515f03ac655c7a4a82ecfc5e0b9636750645f52b
alishabirat <alishabirat@gmail.com>
2025-07-23 09:14:02 +0545

    Add and export icon  from Figma

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,7 +1,21 @@
-export * from "./facebook.svg";
-export * from "./instagram.svg";
-export * from "./twitter.svg";
-export * from "./search.svg";
-export * from "./cart.svg";
-export * from "./wishlist.svg";
-export * from "./user.svg";
+export { default as FacebookIcon } from "./facebook.svg";
+export { default as InstagramIcon } from "./instagram.svg";
+export { default as TwitterIcon } from "./twitter.svg";
+export { default as LinkedinIcon } from "./linkedin.svg";
+export { default as YoutubeIcon } from "./youtube.svg";
+export { default as FooterFacebook } from "./footer-facebook.svg";
+export { default as FooterInstagram } from "./footer-insta.svg";
+export { default as SearchIcon } from "./search.svg";
+export { default as CartIcon } from "./cart.svg";
+export { default as WishlistIcon } from "./wishlist.svg";
+export { default as UserIcon } from "./user.svg";
+export { default as LogoIcon } from "./logo.svg";
+export { default as LocationIcon } from "./location.svg";
+export { default as CallIcon } from "./call.svg";
+export { default as MailIcon } from "./mail.svg";
+
+// HomePageCard icons
+export { default as CardIcon1 } from "./cardicon1.svg";
+export { default as CardIcon2 } from "./cardicon2.svg";
+export { default as CardIcon3 } from "./cardicon3.svg";
+export { default as CardIcon4 } from "./cardicon4.svg";
