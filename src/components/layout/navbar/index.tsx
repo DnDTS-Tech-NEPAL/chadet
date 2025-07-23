01a44924f53fe6da7b61e97d682364a0442cc85d
alishabirat <alishabirat@gmail.com>
2025-07-23 09:47:09 +0545

    Update index.tsx
    update navbar

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -4,16 +4,15 @@ const navItems = ["Shop", "Lipstick", "Eyeliner", "About Us", "Contact"];
 
 export const NavBar = () => {
   return (
-    <Box bg="#FFFAE2" px={6} py={3}>
+    <Box bg="#FFFAE2" px={6} py={6}>
       <Flex gap={8} justify="center">
         {navItems.map((item) => (
           <Text
             key={item}
-            fontSize="sm"
+            fontSize="md"
             color="#4E4934"
             fontWeight="medium"
             cursor="pointer"
-            _hover={{ textDecoration: "underline" }}
           >
             {item}
           </Text>
